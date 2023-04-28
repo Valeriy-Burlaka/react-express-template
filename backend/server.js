fa535b8140dd3f7408876699ec62e8c6a561334e
Valeriy Burlaka <valeriy.burlaka88@gmail.com>
2023-04-28 11:28:17 +0200

    [template] express server

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const messages = [
+  { text: 'Hi there!', author: 'Unknown', timestamp: new Date() },
+];
+
+app.get('/messages', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  
+  return res.json(messages);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
