bdf0c87872ee059b12a5be5331e6d90c9cff927d
Valeriy Burlaka <valeriy.burlaka88@gmail.com>
2023-04-28 13:15:19 +0200

    [server] add POST /messages

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,40 @@
-import express from 'express';
+import express, {
+  type Request,
+  type Response,
+} from 'express';
+
+import { idGenerator as id } from './utils/id';
+
+import { type Message } from './types';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+
 const messages = [
-  { text: 'Hi there!', author: 'Unknown', timestamp: new Date() },
-  { text: 'Any news?', author: 'Unknown', timestamp: new Date() },
+  { id: id(), text: 'Hi there!', author: 'Unknown', timestamp: Date.now() },
+  { id: id(), text: 'Any news?', author: 'Unknown', timestamp: Date.now() },
 ];
 
-app.get('/messages', (req, res) => {
+app.get('/messages', (req: Request, res: Response) => {
   res.setHeader('Content-Type', 'application/json');
   
   return res.json(messages);
 });
+
+app.post('/messages', (req: Request, res: Response) => {
+  const newMessage: Message = {
+    id: id(),
+    text: req.body.text,
+    author: req.body.author,
+    timestamp: req.body.timestamp || Date.now(),
+  };
+  messages.push(newMessage);
+
+  return res.status(201).json(newMessage);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
