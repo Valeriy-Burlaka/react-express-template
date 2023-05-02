8333ae769bdd6c0d0b772bc95a27e068a2216330
Valeriy Burlaka <valeriy.burlaka88@gmail.com>
2023-05-02 09:32:45 +0200

    [client] add basic styling for Messages

--- frontend/src/components/Messages.tsx
+++ frontend/src/components/Messages.tsx
@@ -6,8 +6,17 @@ export function Messages({ messages }: { messages: Message[] }) {
   return (
     <div>
       {messages.map((message) => (
-        <div key={message.id}>
-          <p>{message.text} {`(by ${message.author})`}</p>
+        <div
+          key={message.id}
+          className="bg-white shadow-md rounded sm:rounded-sm px-4 py-2 mb-4 overflow-hidden"
+        >
+          {/* <p>{message.text} {`(by ${message.author})`}</p> */}
+          <p className="text-xl font-semibold">
+            {message.text}
+          </p>
+          <p className="text-sm text-gray-600">
+            {`(by ${message.author})`}
+          </p>
         </div>
       ))}
     </div>
